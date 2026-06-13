991d04ba02bd42541d61c16cf0c46d901aafc8c7
shopmyseason <charisse.tay@gmail.com>
2026-06-13 18:04:11 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,26 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "m.media-amazon.com",
-        pathname: "/images/I/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images-eu.ssl-images-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images-fe.ssl-images-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.ssl-images-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.media-amazon.com",
       },
     ],
   },
